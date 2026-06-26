530311b4f20a95923436c5c6858883d6b090c295
Yuan-cheng-luo <Aly1xbot@outlook.com>
2026-06-26 20:37:47 +0800

    control unit update

---
+++ src/cu.sv
@@ -0,0 +1,48 @@
+module control(
+    input logic [6:0] op,
+    input logic [2:0] func3,
+    input logic [6:0]func7,
+    input logic alu_zero,
+
+    output logic [2:0] ALUcontrol,
+    output logic [1:0] imm_src,
+    output logic regwrite,
+    output logic memwrite
+    
+);
+
+//main decoder
+logic [1:0] alu_op;
+always_comb begin
+    // lw command
+    case(op)
+        7'b0000011: begin
+            regwrite = 1'b1;
+            imm_src = 2'b00;
+            memwrite = 1'b0;
+            alu_op = 2'b00;
+        
+        end
+
+        default: begin
+            regwrite = 1'b0;
+            imm_src = 2'b00;
+            memwrite = 1'b0;
+            alu_op = 2'b00;
+        end
+    endcase
+end
+
+always_comb begin 
+    case(alu_op)
+        //lw, sw
+        2'b00 : ALUcontrol = 3'b000;
+        default : ALUcontrol = 3'b111;
+    endcase
+
+end
+
+
+
+
+endmodule
---
+++ tb/cu_module/test_cu.py
@@ -0,0 +1,35 @@
+import cocotb
+from cocotb.triggers import Timer, RisingEdge
+from cocotb.clock import Clock 
+import numpy as np
+import random
+from cocotb.handle import LogicArrayObject
+
+
+@cocotb.test()
+async def set_unknown (dut):
+    await Timer(1, unit = "ns")
+    #set all input to unkwon before each test
+    dut.op.value = bin(LogicArrayObject("XXXXXXX"))
+    # Uncomment the following throughout the course when needed
+    #
+    # dut.func3.value = bin(LogicArrayObject("XXXX"))
+    # dut.func7.value = bin(LogicArrayObject("XXXXXXX")) added one more X in the string 
+    # dut.alu_zero.value = bin(LogicArrayObject("X"))
+    # dut.alu_last_bit.value = bin(LogicArrayObejct("X"))
+
+    await Timer (1, unit = "ns")
+@cocotb.test()
+async def test_cu (dut):
+    await set_unknown(dut)
+    await Timer(1, unit = "ns")
+    dut.op.value = 0b0000011
+    assert dut.regwrite.value == "1"
+    assert dut.imm_src.value == "00"
+    assert dut.memwrite.value == "0"
+    assert dut.ALUcontrol.value =="000"
+
+
+
+
+
